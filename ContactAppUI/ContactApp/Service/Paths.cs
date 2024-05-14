01147160e172c79dd6b6b57dbcad16e5eeeb68e6
Renata <darknessneteru@gmail.com>
2024-05-14 22:12:52 +0700

    пересоздан класс Paths

--- ContactAppUI/ContactApp/Service/Paths.cs
+++ ContactAppUI/ContactApp/Service/Paths.cs
@@ -1,12 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ContactApp.Service
 {
-	internal class Paths
+	/// <summary>
+	/// Класс путей сохранения файлов сериалайзера.
+	/// </summary>
+	public static class Paths
 	{
+		/// <summary>
+		/// Путь к файлу для сохранения рабочих данных.
+		/// </summary>
+		public static readonly string PathToFiles =
+			Path.Combine(AppContext.BaseDirectory, "Contacts.json");
+
+		/// <summary>
+		/// Путь к файлу для сохранения тестовых данных.
+		/// </summary>
+		public static readonly string PathToFilesTest =
+			Path.Combine(AppContext.BaseDirectory, "ContactsTest.json");
+
+		/// <summary>
+		/// Путь к файлу для сохранения тестовых данных.
+		/// </summary>
+		public static readonly string PathToStandard =
+			Path.Combine(AppContext.BaseDirectory, "Эталон.json");
 	}
 }
